ddb9558ce64d9e8fecfbc6261c999579b5808c7a
Jonathan Scattolini <jscattolini@gmail.com>
2022-09-30 00:51:09 +0200

    Update HTML & CSS & JS Formulaire / Add Privacy&CGV HTML&CSS

--- formulaire.js
+++ formulaire.js
@@ -1,9 +1,9 @@
 const form = document.querySelector(".form");
-const userName = document.querySelector("#formulaireName");
+const userName = document.querySelector("#userName");
 
 
 form.onsubmit = function (event) {
     event.preventDefault();
-    window.alert(`Merci, ${userName.value}, votre message est bien envoyé ! Je vous promet d'y répondre au plus vite !`);
+    window.alert(`Merci ${userName.value} ! Votre message est bien envoyé ! Je vous promet d'y répondre au plus vite !`);
 };
 
