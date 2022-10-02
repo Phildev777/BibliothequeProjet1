01a160db7c3331d09f2d14c4735f91ea52e3c599
Jonathan Scattolini <jscattolini@gmail.com>
2022-10-02 22:50:15 +0200

    update footer avec logo

--- formulaire.js
+++ formulaire.js
@@ -1,37 +1,27 @@
 
 
-/* Evite d'envoyer le popup sans remplir le form mais envoi le pop up et le ferme direct */
 /*
-const form = document.querySelector(".form");
-
-form.onsubmit = function openForm() {
-    openForm.preventDefault();
-    document.getElementById("popupForm").style.display = "block";
-    document.querySelector(".form").style.display = "none";
-}
-
-function closeForm() {
-
-    document.getElementById("popupForm").style.display = "none";
-    document.querySelector(".form").style.display = "initial";
-}
+    openForm.preventDefault();  
 */
 
 /* Envoi le popup mais sans avoir à remplir le formulaire */
 
+
 function openForm() {
+
     document.getElementById("popupForm").style.display = "block";
-    document.querySelector(".form").style.display = "none";
+    document.querySelector(".form").style.opacity = "20%";
     document.querySelector(".googleMap").style.display = "none";
+    document.getElementById("contactMe").style.opacity = "20%";
     document.querySelector(".articles").style.opacity = "20%";
-    document.getElementById("contactMe").style.display = "none";
-}
+};
+
 
 function closeForm() {
 
     document.getElementById("popupForm").style.display = "none";
-    document.querySelector(".form").style.display = "initial";
+    document.querySelector(".form").style.opacity = "initial";
     document.querySelector(".googleMap").style.display = "flex";
+    document.getElementById("contactMe").style.opacity = "initial";
     document.querySelector(".articles").style.opacity = "initial";
-    document.getElementById("contactMe").style.display = "initial";
-}
+};
