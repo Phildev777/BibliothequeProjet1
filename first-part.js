9841ff285eff43d39a061933ebd52bdf03c5ab68
Guillaume Pichaud <guillaume.pichaud44590@gmail.com>
2022-10-03 09:28:44 +0200

    modification de la function readMore en plus simple

--- first-part.js
+++ first-part.js
@@ -1,57 +1,26 @@
 /* lire plus / lire Moins */
 
+let noOfCharac = 150;
+let contents = document.querySelectorAll(".content");
 
-function textReveal2() {
+contents.forEach(content => {
+    let displayText = content.textContent.slice(0, noOfCharac);
+    let moreText = content.textContent.slice(noOfCharac);
+    content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+});
+const lirePlus = "Lire plus +";
+const lireMoins = "Lire moins -";
 
-    const dots = document.getElementById("dots2");
-    const moreText = document.getElementById("more2");
-    const btnText = document.getElementById("lirePlus2");
+function readMore(div) {
+    let post = div.parentElement;
+    let divContent = div.textContent;
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Lire plus <i class='fa-solid fa-plus'></i>";
-        moreText.style.display = "none";
-
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Lire moins <i class='fa-solid fa-minus'></i>";
-        moreText.style.display = "inline";
-
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    if (divContent === lirePlus) {
+        div.innerHTML = lireMoins;
     }
-}
-function textReveal3() {
-
-    const dots = document.getElementById("dots3");
-    const moreText = document.getElementById("more3");
-    const btnText = document.getElementById("lirePlus3");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Lire plus <i class='fa-solid fa-plus'></i>";
-        moreText.style.display = "none";
-
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Lire moins <i class='fa-solid fa-minus'></i>";
-        moreText.style.display = "inline";
-
-    }
-}
-function textReveal1() {
-
-    const dots = document.getElementById("dots1");
-    const moreText = document.getElementById("more1");
-    const btnText = document.getElementById("lirePlus1");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Lire plus <i class='fa-solid fa-plus'></i>";
-        moreText.style.display = "none";
-
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Lire moins <i class='fa-solid fa-minus'></i>";
-        moreText.style.display = "inline";
-
-    }
+    else {
+        div.innerHTML = lirePlus;
+    };
 }
