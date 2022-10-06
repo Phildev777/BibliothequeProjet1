14d41ff96a781a0cd795634ea31f9e1ac21d37d0
NaoSan9 <naomi.santos@outlook.fr>
2022-10-06 11:14:20 +0200

    Animations wooooooork

--- animations.js
+++ animations.js
@@ -1,17 +1,44 @@
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const animation = entry.target.querySelector('.pathDrawingLine');
+const observer = new IntersectionObserver(animations => {
+    animations.forEach(animation => {
+        const svg = animation.target.firstElementChild;
+        const path = svg.firstElementChild;
+        if (animation.isIntersecting) {
 
-        if (entry.isIntersecting) {
-            animation.classList.add('drawingLineAnimation');
+            path.classList.add('drawingLineAnimation');
             return;
+        } else {
+
+            path.classList.remove("drawingLineAnimation");
         }
 
-        animation.classList.remove('drawingLineAnimation');
-    });
-});
+    })
+}, {
+    threshold: [0.5]
+}
+
+)
+
+const animations = document.querySelectorAll(".drawingLineSeparator")
+animations.forEach((animation) => {
+    observer.observe(animation);
+})
 
 
-observer.observe(document.querySelector('.drawingLineSeparator'));
 
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         const animation = entry.target.querySelector('.pathDrawingLine');
+
+//         if (entry.isIntersecting) {
+//             animation.classList.add('drawingLineAnimation');
+//             return;
+//         }
+
+//         animation.classList.remove('drawingLineAnimation');
+//     });
+// });
+
+
+// observer.observe(document.querySelector('.drawingLineSeparator'));
